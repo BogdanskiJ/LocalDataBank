ddb301faff2a138de12c5f14c77f47c405ffa911
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-06-21 17:17:45 +0200

    delete unnecessary comments

--- src/features/manyVariables/getApi.js
+++ src/features/manyVariables/getApi.js
@@ -13,7 +13,7 @@ export const getCategory = async () => {
     }
     return await response.json()
   } catch (error) {
-    return console.log('error in getCategory', error)
+    return console.log('error', error)
   }
 }
 
@@ -25,7 +25,7 @@ export const getGroup = async categoryName => {
     }
     return await response.json()
   } catch (error) {
-    return console.log('error in getGroup', error)
+    return console.log('error', error)
   }
 }
 
@@ -37,7 +37,7 @@ export const getSubGroup = async groupName => {
     }
     return await response.json()
   } catch (error) {
-    return console.log('error in getSubGroup', error)
+    return console.log('error', error)
   }
 }
 
@@ -51,7 +51,7 @@ export const getVariables = async subGroupName => {
     }
     return await response.json()
   } catch (error) {
-    return console.log('error in getVariables', error)
+    return console.log('error', error)
   }
 }
 
@@ -61,10 +61,6 @@ export const getFinalData = async (
   selectedUnitLevel,
 ) => {
   try {
-    console.log('variablesName', variablesName)
-    console.log('selectedUnitLevel', selectedUnitLevel)
-    // const variablesNames = `var-id=${variablesName.value}`
-
     const response = await fetch(
       selectedUnitLevel === '0' || selectedUnitLevel === '2'
         ? `${apiLinkHead}/data/by-variable/${variablesName.value}?unit-level=${selectedUnitLevel}&page=0&page-size=100`
@@ -76,18 +72,6 @@ export const getFinalData = async (
     }
     return await response.json()
   } catch (error) {
-    return console.log('error in getFinalData', error)
+    return console.log('error', error)
   }
 }
-
-// https://bdl.stat.gov.pl/api/v1/data/by-variable/148190?unit-level=0&page=0&page-size=100
-// ostateczne zapytanie dla Polski
-
-// zapytanie dla województw - wyświetlają się województwa
-// https://bdl.stat.gov.pl/api/v1/data/by-variable/148190?unit-level=2&page=0&page-size=100
-
-// wybierając województwo - lista powiatów z danymi
-//https://bdl.stat.gov.pl/api/v1/data/by-variable/148190?unit-parent-id=012400000000&unit-level=5&page=0&page-size=100
-
-// wybierając powiat - lista gmin z danymi
-// https://bdl.stat.gov.pl/api/v1/data/by-variable/148190?unit-parent-id=012414402000&unit-level=6&page=0&page-size=100
