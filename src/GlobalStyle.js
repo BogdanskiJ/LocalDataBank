b46747894d1f9f936f321d3f16dcd0c74e415aa3
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-06-21 10:37:41 +0200

    custom scrollbar

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,17 +1,35 @@
-import { createGlobalStyle } from "styled-components";
+import {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;
     };
-
     *,::after, ::before {
         box-sizing: inherit;
     };
 
     body {
         margin: 0 auto;
-        background: ${({ theme }) => theme.color.whisper};
+        background: ${({theme}) => theme.color.whisper};
         font-family: "Roboto", sans-serif;
     };
-`;
+    
+    body::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+    }
+    body::-webkit-scrollbar-track {
+        border-radius: 10px;
+        background: rgba(0,0,0,0.1);
+    }
+    body::-webkit-scrollbar-thumb{
+        border-radius: 10px;
+        background: rgba(0,0,0,0.2);
+    }
+    body::-webkit-scrollbar-thumb:hover{
+  	    background: rgba(0,0,0,0.4);
+    }
+    body::-webkit-scrollbar-thumb:active{
+  	    background: rgba(0,0,0,0.5);
+    }
+`
