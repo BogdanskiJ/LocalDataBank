0eaa9795cc03de7b9ba9d870e55a2122cf4396c1
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-06-21 17:59:13 +0200

    remove unnecessary saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,12 +1,10 @@
 import {all} from '@redux-saga/core/effects'
-import {mapsSaga} from './features/maps/mapsSaga'
 import {teritorialUnitSaga} from './features/teritorialUnit/teritorialUnitSaga'
 import {manyVariablesSaga} from './features/manyVariables/manyVariablesSaga'
 export default function* rootSaga() {
   yield all([
     teritorialUnitSaga(),
     manyVariablesSaga(),
-    mapsSaga(),
     // watchFetchPeopleList(),
     // watchPeopleDetails(),
     // watchFetchProjects(),
