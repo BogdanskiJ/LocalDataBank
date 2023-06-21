1ce1116d2b1142b51217874a4814672616f2f314
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-06-21 10:50:09 +0200

    add props to NoDataPage and set zeroState on chang

--- src/features/teritorialUnit/index.js
+++ src/features/teritorialUnit/index.js
@@ -1,4 +1,4 @@
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import TeritorialUnit from './teritorialUnit'
 import {MapPoland} from '../maps/MapPoland'
 import {StyledHead, StyledPageBody, StyledResults} from './styled'
@@ -6,12 +6,24 @@ import {
   selectProvinceName,
   selectTeritorialUnitFinalData,
   selectTeritorialUnitStatus,
+  selectTeritorialUnitSubGroupData,
+  selectTeritorialUnitSubGroupName,
   selectTeritorialUnitVariablesName,
+  setProvinceNameBegin,
+  setTeritorialUnitCategoryName,
 } from './teritorialUnitSlice'
 import {Results} from './displayingResults'
 import NoDataPage from '../../common/NoDataPage'
 import {MapProvinces} from '../maps/MapProvinces'
 import ErrorPage from '../../common/ErrorPage'
+import {useState} from 'react'
+import {useEffect} from 'react'
+import {useLocation} from 'react-router-dom'
+import {
+  setManyVariablesBegin,
+  setProvinceNameVariablesBegin,
+} from '../manyVariables/manyVariablesSlice'
+import {setSelectedMapBegin} from '../maps/mapsSlice'
 
 function TeritorialUnitManyVariables() {
   const teritorialUnitFinalData = useSelector(selectTeritorialUnitFinalData)
@@ -20,6 +32,20 @@ function TeritorialUnitManyVariables() {
   )
   const provinceName = useSelector(selectProvinceName)
   const status = useSelector(selectTeritorialUnitStatus)
+
+  const teritorialUnitSubGroupName = useSelector(
+    selectTeritorialUnitSubGroupName,
+  )
+  const teritorialUnitSubGroupData = useSelector(
+    selectTeritorialUnitSubGroupData,
+  )
+  const dispatch = useDispatch()
+  const {pathname} = useLocation()
+  useEffect(() => {
+    pathname === '/jednostki-terytorialne'
+      ? dispatch(setManyVariablesBegin()) && dispatch(setSelectedMapBegin())
+      : ''
+  }, [])
 
   return (
     <StyledPageBody>
@@ -38,7 +64,10 @@ function TeritorialUnitManyVariables() {
               {teritorialUnitFinalData !== '' ? (
                 <div>
                   {teritorialUnitFinalData.totalRecords === 0 ? (
-                    <NoDataPage />
+                    <NoDataPage
+                      data={teritorialUnitSubGroupData}
+                      names={teritorialUnitSubGroupName.value}
+                    />
                   ) : (
                     <Results />
                   )}
