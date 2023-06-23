41191bb90e3005130c4b8226b83fe5746e38aa07
Jakub Bogdanski <bogdanskikuba@gmail.com>
2023-06-23 13:24:57 +0200

    new styling

--- src/features/maps/MapPoland/styled.js
+++ src/features/maps/MapPoland/styled.js
@@ -90,6 +90,7 @@ export const StyledCheck = styled.div`
 
 export const StyledSelect = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 5px;
   justify-content: center;
   align-items: center;
